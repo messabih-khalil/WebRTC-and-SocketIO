cd15559c7c39ebbaebed7ab1a4de02263c54a6db
messabih-khalil <msbih.khalil@gmail.com>
2023-05-14 22:17:26 +0100

    feat(server.js): add MongoDB connection before running the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,20 @@
 // Active Envirement Variables
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 const PORT = process.env.PORT || process.env.APP_PORT;
 
-// Run Server
+// Connect To MongoDB and Run Server
 
-app.listen(PORT, () => {
-  console.log('Server Is Running In Port : ', PORT);
-});
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Server Is Running In Port : ', PORT);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
