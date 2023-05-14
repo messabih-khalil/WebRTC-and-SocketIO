0faddc45d3d66b4697e52cfd76d16d10bf948fdc
messabih-khalil <msbih.khalil@gmail.com>
2023-05-14 22:14:05 +0100

    feat(postLogin.js): add postLogin controller for user authentication

---
+++ Backend/controllers/auth/postLogin.js
@@ -0,0 +1,30 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+const JWT = require('jsonwebtoken');
+const postLogin = async (req, res) => {
+  try {
+    const { mail, password } = req.body;
+    // Check if User Exist
+
+    const user = await User.findOne({ mail: mail.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Generate Token
+      const token = await JWT.sign(user.id, process.env.JWT_SECERT_KEY);
+      // Response
+      return res.status(200).json({
+        success: true,
+        token: token,
+      });
+    }
+
+    return res.status(200).json({
+      success: false,
+      message: 'Invalid Credentials. PLease Try again',
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = postLogin;
