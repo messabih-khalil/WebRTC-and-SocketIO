a6e686a41e414d9dad65846389afa90f97dd2296
messabih-khalil <msbih.khalil@gmail.com>
2023-05-14 22:17:40 +0100

    feat(app.js): add auth routes to the app

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 // Initialize App
 const app = express();
 // App Middleware
 app.use(express.json());
 app.use(cors());
 
+// App Routes
+
+app.use('/api/auth', require('./routes/auth.routes'));
 
 module.exports = app;
