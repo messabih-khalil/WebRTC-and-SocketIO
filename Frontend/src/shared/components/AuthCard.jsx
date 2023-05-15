c95807db2a1e7f98446849b27ebbe6d3ae1b7e20
messabih-khalil <msbih.khalil@gmail.com>
2023-05-16 00:58:21 +0100

    style(AuthCard.jsx): remove height property from AuthBox component

--- Frontend/src/shared/components/AuthCard.jsx
+++ Frontend/src/shared/components/AuthCard.jsx
@@ -16,7 +16,6 @@ const AuthBox = ({ children }) => {
       <Box
         sx={{
           width: 700,
-          height: 400,
           bgcolor: '#36393f',
           borderRadius: '5px',
           boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
