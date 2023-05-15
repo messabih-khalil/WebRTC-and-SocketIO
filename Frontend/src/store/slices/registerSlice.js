123111d5a8bb36b399204c7a351aac53014e0a26
messabih-khalil <msbih.khalil@gmail.com>
2023-05-16 00:59:26 +0100

    feat(registerSlice.js): add extraReducers to handle register.fulfilled action and redirect to home page after successful registration refactor(registerSlice.js): rename setEmail to setRegisterEmail and setPassword to setRegisterPassword for clarity

--- Frontend/src/store/slices/registerSlice.js
+++ Frontend/src/store/slices/registerSlice.js
@@ -1,26 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import { register } from '../thunks/register';
+import { redirect } from 'react-router-dom';
 const registerSlice = createSlice({
   name: 'login',
   initialState: {
-    error: '',
     username: '',
     email: '',
     password: '',
+    token: '',
   },
   reducers: {
     setUsername(state, action) {
       state.username = action.payload;
     },
-    setEmail(state, action) {
+    setRegisterEmail(state, action) {
       state.email = action.payload;
     },
-    setPassword(state, action) {
+    setRegisterPassword(state, action) {
       state.password = action.payload;
     },
   },
+  extraReducers(builder) {
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.token = action.payload;
+      return redirect('/');
+    });
+  },
 });
 
-export const { setEmail, setPassword, setUsername } = registerSlice.actions;
+export const { setRegisterEmail, setRegisterPassword, setUsername } = registerSlice.actions;
 
 export const registerReducer = registerSlice.reducer;
