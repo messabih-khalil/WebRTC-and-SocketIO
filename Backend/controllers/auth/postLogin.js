a0db1af67ca18802e9502675f3f5c18bda752f1a
messabih-khalil <msbih.khalil@gmail.com>
2023-05-15 22:27:41 +0100

    fix(postLogin.js): change response status code from 200 to 400 when credentials are invalid

--- Backend/controllers/auth/postLogin.js
+++ Backend/controllers/auth/postLogin.js
@@ -18,7 +18,7 @@ const postLogin = async (req, res) => {
       });
     }
 
-    return res.status(200).json({
+    return res.status(400).json({
       success: false,
       message: 'Invalid Credentials. PLease Try again',
     });
