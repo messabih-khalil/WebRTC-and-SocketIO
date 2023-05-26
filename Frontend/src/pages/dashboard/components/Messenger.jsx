5c8c5e1167ff8f0cdef8fbe5f0bdcbcdece51b2a
messabih-khalil <msbih.khalil@gmail.com>
2023-05-26 17:48:14 +0100

    feat(Messenger.jsx): add Messenger component to dashboard page

---
+++ Frontend/src/pages/dashboard/components/Messenger.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const MainContainer = styled('div')({
+  flexGrow: 1,
+  backgroundColor: '#36393f',
+  marginTop: '48px',
+  display: 'flex',
+});
+
+const Messenger = () => {
+  return <MainContainer></MainContainer>;
+};
+
+export default Messenger;
