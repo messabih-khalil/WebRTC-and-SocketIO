3a06d1700b919f773e448f102ccd644e59cffaf3
messabih-khalil <msbih.khalil@gmail.com>
2023-05-26 16:36:14 +0100

    feat(httpClient.js): add authorization header to httpClient requests using token from localStorage

--- Frontend/src/utils/httpClient.js
+++ Frontend/src/utils/httpClient.js
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const httpClient = axios.create({
   baseURL: 'http://localhost:3232/api',
 });
+
+httpClient.interceptors.request.use(config => {
+  let token = localStorage.getItem('token');
+
+  if (token) {
+    token = JSON.parse(token).token;
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
