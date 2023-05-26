48d067adfa65628aaca74b9972b2dec0d13bd62e
messabih-khalil <msbih.khalil@gmail.com>
2023-05-26 17:49:46 +0100

    feat(dashboard): add SideBar, FriendsSideBar, Messenger and AppBar components to dashboard page

--- Frontend/src/pages/dashboard/index.jsx
+++ Frontend/src/pages/dashboard/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { logout } from '../../utils/logout';
+import { styled } from '@mui/system';
+import SideBar from './components/SideBar';
+import FriendsSideBar from './components/FriendsSideBar';
+import Messenger from './components/Messenger';
+import AppBar from './components/AppBar';
+
+const Wrapper = styled('div')({
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+});
 
 const DashboardPage = () => {
   return (
-    <div>
-      Dashboard
-      <button onClick={() => logout()}>LogOut</button>
-    </div>
+    <Wrapper>
+      <SideBar />
+      <FriendsSideBar />
+      <Messenger />
+      <AppBar />
+    </Wrapper>
   );
 };
 
